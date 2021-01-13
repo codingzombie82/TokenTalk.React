ddc6de0aacbec5238ab2dd7d915132756fb18d6d
이진영 <devjinjin@gmail.com>
2021-01-14 00:55:54 +0900

    샘플 모델 클래스 적용 , 데이터 베이스 Talks와 1대1 매칭

---
+++ TokenTalk.Models/Talk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TokenTalk.Models
+{
+    public class BoardBase
+    {
+        /// <summary>
+        /// 일련번호
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "번호")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 제목
+        /// </summary>
+        /// 
+        [MaxLength(255)]
+        [Required(ErrorMessage = "필수 입력사항 입니다.")]
+        [Display(Name = "글 제목"]
+        [Column(TypeName = "NVarChar(255)")]
+        public string Title { get; set; }
+    
+        /// <summary>
+        /// 내용
+        /// </summary>
+        /// 
+        [Display(Name ="내용")]
+        public string Description { get; set; }
+    }
+
+    [Table("Talks")]
+    public class Talk : BoardBase
+    {
+        
+
+    }
+}
