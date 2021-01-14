780ec18b05c047f52eb19d969d9c5045f4c06c04
이진영 <devjinjin@gmail.com>
2021-01-14 17:16:11 +0900

    Web Api를 React 프로젝트로 옮김

--- TokenTalk.React/Startup.cs
+++ TokenTalk.React/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TokenTalk.Models;
 
 namespace TokenTalk.React
 {
@@ -23,11 +25,27 @@ namespace TokenTalk.React
 
             services.AddControllersWithViews();
 
+            // TalkApp 관련 의존성(종속성) 주입 관련 코드만 따로 모아서 관리 
+            AddDependencyInjectionContainerForTalkApp(services);
+
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
+        }
+
+        /// <summary>
+        /// TalkApp 관련 의존성(종속성) 주입 관련 코드만 따로 모아서 관리 
+        /// </summary>
+        private void AddDependencyInjectionContainerForTalkApp(IServiceCollection services)
+        {
+            // TalkAppDbContext.cs Inject: New DbContext Add
+            services.AddEntityFrameworkSqlServer().AddDbContext<TalkAppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // ITalkRepository.cs Inject: DI Container에 서비스(리포지토리) 등록 
+            services.AddTransient<ITalkRepository, TalkRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
