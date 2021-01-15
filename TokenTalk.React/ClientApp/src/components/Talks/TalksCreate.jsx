12b3bf19d4e31dcaf3749b3bb78e40e4000d2771
이진영 <devjinjin@gmail.com>
2021-01-15 12:46:02 +0900

    데이터 수정 기능 추가

--- TokenTalk.React/ClientApp/src/components/Talks/TalksCreate.jsx
+++ TokenTalk.React/ClientApp/src/components/Talks/TalksCreate.jsx
@@ -6,24 +6,86 @@ export class TalksCreate extends Component {
         super(props);
 
         this.state = {
-            Talks: [],
-            loading: true,
+            title: '',
+            description : '',
+            created : null
         };
-
+        this.handleChangeTitle = this.handleChangeTitle.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.goIndex = this.goIndex.bind(this);
     }
     componentDidMount() {
         
     }
+    handleChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        });
+    }
+    handleChangeTitle(e){
+        this.setState({
+            title: e.target.value
+        });
+    }
+
+    goIndex(){
+        const { history } = this.props;        
+        history.push("/Talks");
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
 
 
 
-    
+        let talkData = {
+            title : this.state.title,
+            description : this.state.description
+        };
+
+        axios.post('/api/Talks', talkData).then(result => {
+            this.goIndex();
+        });
+    }
 
     render() {
 
         return (
             <div>
                 <h1>TalksCreate</h1>
+
+                <div className="row">
+                    <div className="col-md-8">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label>Title</label>
+                                <input 
+                                type="text" 
+                                className="form-control" 
+                                value={this.state.title} 
+                                placeholder="제목을 입력하세요"
+                                onChange={this.handleChangeTitle}
+                                ></input>
+                            </div>
+                            <div className="form-group">
+                                <label>Description</label>
+                                <input 
+                                type="text" 
+                                className="form-control" 
+                                value={this.state.description} 
+                                placeholder="내용을 입력하세요"
+                                onChange={this.handleChangeDescription}
+                                ></input>
+                            </div>
+                            <div className="form-group">
+                                <button className="btn btn-secondary" onClick={this.goIndex}>List</button>
+                                &nbsp;
+                                <button className="btn btn-primary" type="submit">저장</button>
+                            </div>
+                        </form>                        
+                    </div>
+                </div>
             </div>
         );
     }
--- TokenTalk.React/ClientApp/src/components/Talks/TalksEdit.jsx
+++ TokenTalk.React/ClientApp/src/components/Talks/TalksEdit.jsx
@@ -6,24 +6,102 @@ export class TalksEdit extends Component {
         super(props);
 
         this.state = {
-            Talks: [],
-            loading: true,
+            title: '',
+            description: '',
+            created: null
+        };
+        this.handleChangeTitle = this.handleChangeTitle.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.goIndex = this.goIndex.bind(this);
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        // id 값에 해당하는 단일 데이터를 web api로 부터 읽기
+        console.log(id);
+
+        axios.get("/api/Talks/" + id).then(response => {
+            const data = response.data;
+
+            this.setState({
+                title: data.title,
+                description: data.description,
+                created: data.created
+            });
+        });
+    }
+
+    handleChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    handleChangeTitle(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+
+    goIndex() {
+        const { history } = this.props;
+        history.push("/Talks");
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const { id } = this.props.match.params;
+
+        let talkData = {
+            title: this.state.title,
+            description: this.state.description,
+            created: this.state.created
         };
 
+        axios.put('/api/Talks/' + id, talkData).then(result => {
+            this.goIndex();
+        });
     }
-    componentDidMount() {
-
-    }
-
-
-
-    
 
     render() {
 
         return (
             <div>
                 <h1>TalksEdit</h1>
+
+                <div className="row">
+                    <div className="col-md-8">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label>Title</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    value={this.state.title}
+                                    placeholder="제목을 입력하세요"
+                                    onChange={this.handleChangeTitle}
+                                ></input>
+                            </div>
+                            <div className="form-group">
+                                <label>Description</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    value={this.state.description}
+                                    placeholder="내용을 입력하세요"
+                                    onChange={this.handleChangeDescription}
+                                ></input>
+                            </div>
+                            <div className="form-group">
+                                <button className="btn btn-secondary" onClick={this.goIndex}>Cancel</button>
+                                &nbsp;
+                                <button className="btn btn-primary" type="submit">저장</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
