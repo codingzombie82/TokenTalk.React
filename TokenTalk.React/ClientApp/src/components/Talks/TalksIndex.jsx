3aed9c00dea18fa65dfb458fa3e0eab4ba63f64b
이진영 <devjinjin@gmail.com>
2021-01-15 11:26:38 +0900

    create, edit, delete 버튼 및 클릭 이벤트 처리기 생성

--- TokenTalk.React/ClientApp/src/components/Talks/TalksIndex.jsx
+++ TokenTalk.React/ClientApp/src/components/Talks/TalksIndex.jsx
@@ -9,45 +9,72 @@ export class TalksIndex extends Component {
             Talks: [],
             loading : true,
         };
+
     }
     componentDidMount() {
         this.populateTalkData();
     }
 
-    static renderTalksTable(talks) {
+
+
+    renderTalksTable(talks) {
         return (
             <table className='table table-striped' aria-labelledby="tabelLabel">
                 <thead>
                     <tr>
                         <th>Id</th>
                         <th>Title</th>
-                        <th>-</th>
-                        <th>-</th>
+                        <th>Description</th>
+                        <th>Created</th>
+                        <th>Action, Admin</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {talks.map(talks =>
-                        <tr key={talks.id}>
-                            <td>{talks.id}</td>
-                            <td>{talks.title}</td>
-                            <td>{talks.title}</td>
-                            <td>{talks.title}</td>
+                    {talks.map(talk =>
+                        <tr key={talk.id}>
+                            <td>{talk.id}</td>
+                            <td>{talk.title}</td>
+                            <td>{talk.description}</td>
+                            <td>{talk.created ? new Date(talk.created).toLocaleDateString() : "-"}</td>
+                            <td className="text-nowrap">
+                                <button className="btn btn-sm btn-primary" onClick={() => this.editBy(talk.id)}>Edit</button>
+                                &nbsp;
+                                <button className="btn btn-sm btn-danger" onClick={() => this.deleteBy(talk.id)}>Delete</button>
+                            </td>
                         </tr>
                     )}
                 </tbody>
             </table>
         );
     }
+    createPage() {
+        console.log('goCreatePage');
+    }
+
+    editBy(id) {
+        console.log('editBy' + id);
+
+    }
+
+    deleteBy(id) {
+        console.log('deleteBy' + id);
+
+    }
 
     render() {
 
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
-            : TalksIndex.renderTalksTable(this.state.Talks);
+            : this.renderTalksTable(this.state.Talks);
 
         return (
             <div>
-                <h1>Talk List</h1>
+                <h1>Talk List &nbsp;
+                    <button className="btn btn-primary" onClick={() => this.createPage()}>
+                        <span className="fa fa-plus">+</span>
+                    </button>
+                </h1>
+
                 <p>This component demonstrates fetching data from the server.</p>
                 {contents}
             </div>
